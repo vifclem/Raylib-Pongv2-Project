4726d72b78c5979098e947395e96a51dded99686
Marie (Aenorya) <marie.legrand@artfx.fr>
2023-09-21 16:40:48 +0200

    [init] VS 2022 project creation with Raylib

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "raylib.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    
+    cout << "Hello World" << endl;
+
+    InitWindow(300, 300, "My first Raylib window!");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false) {
+        BeginDrawing();
+        ClearBackground(DARKGREEN);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
